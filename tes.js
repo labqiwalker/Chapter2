e5964ddc8de2b34413061b42a3a3b224bff92aab
labqiwalker <labqiwalker@gmail.com>
2022-03-09 21:54:37 -0600

    latihan

---
+++ tes.js
@@ -0,0 +1,43 @@
+let sepatuAdi = 32;
+
+switch (sepatuAdi) {
+  case 32:
+    console.log ("ukuran sepatu ready")
+    break;
+  case 23:
+    console.log ("ukuran sepatu ready")
+    break;
+  case 80 :
+    console.log ("ukuran sepatu ready")
+    break;
+  case 54:
+    console.log ("ukuran sepatu ready")
+    break;
+
+  default:
+    console.log ("ukuran sepatu tidak tersedia")
+    break;
+}
+
+let sepatuRahmat = 40;
+
+switch (sepatuRahmat) {
+  case 32:
+    console.log ("ukuran sepatu ready")
+    break;
+  case 23:
+    console.log ("ukuran sepatu ready")
+    break;
+  case 80 :
+    console.log ("ukuran sepatu ready")
+    break;
+  case 54:
+    console.log ("ukuran sepatu ready")
+    break;
+
+  default:
+    console.log ("ukuran sepatu tidak tersedia")
+    break;
+}
+
+
